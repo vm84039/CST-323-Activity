02735de79bd959c8a80839e843f79b88ab15b239
Vinson Martin <vm84039@yahoo.com>
2022-04-15 13:05:57 -0700

    make it better

--- webpack.mix.js
+++ webpack.mix.js
@@ -15,3 +15,11 @@ mix.js('resources/js/app.js', 'public/js')
     .vue()
     .sass('resources/sass/app.scss', 'public/css');
 mix.sass('resources/sass/custom.scss', 'public/css');
+mix.js('resources/js/bootstrap.js', 'public/js');
+mix.js('resources/js/theme.js', 'public/js');
+mix.js('resources/js/vanilla-zoom.js', 'public/js');
+mix.js('resources/js/bootstrap.min.js', 'public/js');
+mix.sass('resources/sass/vanilla-zoom.min.scss', 'public/css');
+
+
+
